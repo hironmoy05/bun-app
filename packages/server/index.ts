e671ac0b820c9474d09236b1703f6aa2a71f4c59
hironmoy05 <hironmoy.dhar@zolvify.com>
2025-11-06 21:46:31 +0530

    Track the last conversation

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import OpenAI from 'openai';
 
 dotenv.config();
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+})
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3001;
 
 app.get('/', (req: Request, res: Response) => {
@@ -14,6 +19,24 @@ app.get('/', (req: Request, res: Response) => {
 app.get('/api/hello', (req: Request, res: Response) => {
   res.json({ message: 'Hello from the API!' });
 });
+
+let lastResponseId: string | null = null;
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+  const { prompt } = req.body
+
+  const response = await client.responses.create({
+    model: 'gpt-5-nano',
+    reasoning: { "effort": "low" },
+    input: prompt,
+    max_output_tokens: 150,
+    previous_response_id: lastResponseId
+  })
+
+  lastResponseId = response.id
+
+  res.json({ message: response.output_text })
+})
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
